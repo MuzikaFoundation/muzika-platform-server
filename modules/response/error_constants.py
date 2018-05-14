49c22582bfef5935425b2a6e05f9db96699dfa40
seungwon-kang <ksw3894@gmail.com>
2018-05-14 14:47:42 +0900

    feature(response): add response helper modules

---
+++ modules/response/error_constants.py
@@ -0,0 +1,7 @@
+
+"""
+ Defines error codes and error messages for the server response.
+"""
+
+INVALID_REQUEST_BODY = 401
+INVALID_REQUEST_BODY_MSG = "Invalid request body."
---
+++ modules/response/helper.py
@@ -0,0 +1,26 @@
+
+from flask import json
+from flask import make_response, jsonify, Response
+
+
+def response_ok(additional_data):
+    json_str = json.dumps(additional_data, ensure_ascii=False).encode('utf-8').decode('utf-8')
+
+    response = make_response(json_str, 200)
+    response.headers['Content-Type'] = 'application/json; charset=utf-8'
+
+    return response
+
+
+def response_xml(xml_str):
+    return Response(response=xml_str,
+                    status=200,
+                    mimetype="application/xml")
+
+
+def response_err(state, message, data=None, http_status_code=406):
+    return make_response(jsonify({
+        'state': state,
+        'message': message,
+        'data': data
+    }), http_status_code)
