b3d0c86d663689e53cbbea4cbcbdc34879da092b
seungwon-kang <ksw3894@gmail.com>
2018-05-17 16:19:09 +0900

    fix(artist-upload): authenticate user by JWT token

--- controllers/artist.py
+++ controllers/artist.py
@@ -3,14 +3,15 @@ from flask import Blueprint, request
 from modules.ipfs import RelayIpfs
 from modules.web3 import get_web3
 from modules.muzika_contract import MuzikaContractHandler
-from modules.signature import validate_signature
 from modules.response import helper
 from modules.response import error_constants as ER
+from modules.login import jwt_check
 
 blueprint = Blueprint('artist', __name__, url_prefix='/api')
 
 
 @blueprint.route('/paper', methods=['PUT'])
+@jwt_check
 def _upload_paper():
     """
     Uploads a paper file from client.
@@ -18,10 +19,8 @@ def _upload_paper():
     json form:
     {
         "hash": file hash string,
-        "address": artist wallet address string (public-key hash),
         "price": uint256,
         "encrypted": true | false,
-        "signature": signature object for validation
     }
 
     Server downloads the paper file by its IPFS process from the client IPFS node and it creates a muzika contract
@@ -31,15 +30,9 @@ def _upload_paper():
     """
     json_form = request.get_json(force=True, silent=True)
     file_hash = json_form.get('hash')
-    wallet_address = json_form.get('address')
+    wallet_address = request.user['address']
     price = json_form.get('price')
     encrypted = json_form.get('encrypted', False)
-    sig_obj = json_form.get('signature')
-
-    # check signature validation
-    web3 = get_web3()
-    if not validate_signature(web3, wallet_address, sig_obj):
-        return helper.response_err(ER.INVALID_SIGNATURE, ER.INVALID_SIGNATURE_MSG)
 
     # check request json
     if not isinstance(file_hash, str) or not isinstance(price, int) or not isinstance(encrypted, bool):
@@ -54,6 +47,7 @@ def _upload_paper():
     api.pin_add(file_hash)
 
     # create a paper contract
+    web3 = get_web3()
     contract_handler = MuzikaContractHandler()
     paper_contract = contract_handler.get_contract(web3, 'MuzikaPaperContract')
 
