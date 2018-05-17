70c3fefef931d26c191cfb11ef08ea835f51a282
seungwon-kang <ksw3894@gmail.com>
2018-05-17 16:09:47 +0900

    refactor(database): remove ORM session maker

--- modules/database.py
+++ modules/database.py
@@ -2,23 +2,20 @@
 """
  Use muzika database.
 
- Use session_scope() for getting a session. It manages transaction and exceptions automatically.
-
- For using read-only database,
- >>> with session_scope() as session:
+ For using database for read-only,
+ >>> with engine_rdonly.connect() as connection:
  >>>     ...
 
- For using readable and writable database,
- >>> with session_scope(writable=True) as session:
+ For using database for read-writable,
+ >>> with engine_rdwr.connect() as connection:
  >>>     ...
+
 """
 
 import os
 import json
-from contextlib import contextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine.url import URL
 
 __all__ = [
@@ -37,26 +34,3 @@ rdwr_db_url = URL(**db_secret['db_rdwr'])
 # define database engines
 engine_rdonly = create_engine(rdonly_db_url, encoding='utf-8', pool_recycle=290)
 engine_rdwr = create_engine(rdwr_db_url, encoding='utf-8', pool_recycle=290)
-
-# define session makers
-session_rdonly = sessionmaker(bind=engine_rdonly)
-session_rdwr = sessionmaker(bind=engine_rdwr)
-
-
-@contextmanager
-def session_scope(writable=False, autocommit=True, autoflush=True):
-    if writable:
-        session = session_rdonly(autocommit=autocommit, autoflush=autoflush)
-    else:
-        session = session_rdwr(autocommit=autocommit, autoflush=autoflush)
-
-    try:
-        yield session
-        if autocommit is False:
-            session.commit()
-    except Exception:
-        if autocommit is False:
-            session.rollback()
-        raise
-    finally:
-        session.close()
