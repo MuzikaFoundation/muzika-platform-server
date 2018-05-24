0656a3c2e904f3c4280b0a21a157eb736ba5c2d7
seungwon-kang <ksw3894@gmail.com>
2018-05-24 17:21:00 +0900

    fix(signature): support several types message

--- modules/signature.py
+++ modules/signature.py
@@ -28,16 +28,25 @@ def validate_signature(web3, address, sig_obj):
     """
     Validate the signature by signature object.
 
-    The signature objects have either "message" or "messageHash" and "signature". It calculates the wallet address from
-    the message hash and signature and check validation by comparing it with original address.
+    It calculates the wallet address from the message hash and signature
+    and check validation by comparing it with original address.
     """
+    from modules.sign_message import construct_sign_message
+
     try:
         purpose = sig_obj.get('purpose')
         message = sig_obj.get('message')
-        message_hash = HexBytes(sig_obj.get('messageHash')) or \
-                       defunct_hash_message(text='{}\nsignature : {}'.format(purpose, message))
-        signature = sig_obj.get('signature')
-        return web3.eth.account.recoverHash(message_hash=message_hash, signature=signature) == address
+        version = sig_obj.get('signature_version', 1)
+        signature = HexBytes(sig_obj.get('signature'))
+
+        muzika_message = construct_sign_message(purpose, message, version)
+        message_hash = defunct_hash_message(web3.sha3(muzika_message))
+
+        # recover address from hash and signature
+        recover_address = web3.eth.account.recoverHash(message_hash, signature=signature)
+
+        # if equal to address, it's valid signature.
+        return recover_address.lower() == address.lower()
     except TypeError:
         return False
     except ValueError:
