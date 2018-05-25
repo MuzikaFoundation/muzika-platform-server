d0421c7a7deb4322b8dc23358f430d4a9fa9b82b
seungwon-kang <ksw3894@gmail.com>
2018-05-25 18:51:51 +0900

    feat(community): add a community post API

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 from config import WebServerConfig
 from controllers import (
-    artist, user, file
+    artist, user, file, community
 )
 from modules.json_encoder import FlaskJSONEncoder
 
@@ -19,6 +19,7 @@ blueprints = [
     artist.blueprint,
     user.blueprint,
     file.blueprint,
+    community.blueprint,
 ]
 
 production_blueprints = [
