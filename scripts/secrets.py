c9896d26115dfac93fe3808e116305f3e918707a
Heo Sangmin <heo@mapiacompany.com>
2018-05-25 22:48:35 +0900

    feature(scripts/secrets): profile option for aws credentials

--- scripts/secrets.py
+++ scripts/secrets.py
@@ -15,12 +15,13 @@ args = argparse.ArgumentParser()
 args.add_argument('--upload', help="Upload secret files to S3 bucket.", default=False, action='store_true')
 args.add_argument('--download', help="Download secret files from S3 bucket.", default=False, action='store_true')
 args.add_argument('--bucket', help="The name of S3 bucket", required=True)
+args.add_argument('--profile', help="The name of aws credentials profile name", default=None)
 args = args.parse_args()
 
 if (args.upload and args.download) or (not args.upload and not args.download):
     print('Select only one of uploading or downloading.')
 
-session = boto3.Session()
+session = boto3.Session() if args.profile is None else boto3.Session(profile_name=args.profile)
 s3 = session.client('s3')
 
 if args.upload:
