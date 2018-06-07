3a20f894568f469ddf163255202ec02a0aff6d23
Heo Sangmin <heo@mapiacompany.com>
2018-06-08 01:55:35 +0900

    fix(file): use request.values for post method body & rename file_hash to ipfs_hash (meaning conflicts)

--- controllers/file.py
+++ controllers/file.py
@@ -18,7 +18,7 @@ def _upload_file():
     uploads a file. (Only one file is allowed at once)
     """
     user_id = request.user['user_id']
-    file_type = request.args.get('type')
+    file_type = request.values.get('type')
 
     # get files
     if len(request.files) != 1:
@@ -31,16 +31,16 @@ def _upload_file():
 
     # get ipfs hash value
     # ipfs = RelayIpfs()
-    # file_hash = ipfs.get_connection().add_bytes(file_data)
+    # ipfs_hash = ipfs.get_connection().add_bytes(file_data)
 
     # TODO: Download file from IPFS and verify file blob is same with encrypted file_data
-    file_hash = request.args.get('file_hash')
-    aes_key = request.args.get('aes')
+    ipfs_hash = request.values.get('ipfs_hash')
+    aes_key = request.values.get('aes')
 
     bucket = MuzikaS3Bucket(file_type=file_type)
     with db.engine_rdwr.begin() as connection:
         file = bucket.put(connection, name=file_name, value=file_data, user_id=user_id,
-                          file_type=file_type, content_type=file.content_type, hash=file_hash, aes_key=aes_key)
+                          file_type=file_type, content_type=file.content_type, hash=ipfs_hash, aes_key=aes_key)
 
         if not file:
             return helper.response_err(ER.UPLOAD_FAIL, ER.UPLOAD_FAIL_MSG)
