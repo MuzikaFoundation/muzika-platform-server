5e5e9ae702848b7a22eded5fc5df9cd67789f5fa
Heo Sangmin <heo@mapiacompany.com>
2018-06-19 12:39:15 +0900

    chore: add exit condition for test code runs

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,20 @@
+import os
+import sys
 import unittest
 
-from tests import db_statement
+from tests import test_db_stmt
 
 # initialize the test suite
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # add tests to the test suite
-suite.addTests(loader.loadTestsFromModule(db_statement))
+suite.addTests(loader.loadTestsFromModule(test_db_stmt))
 
 if __name__ == '__main__':
     # initialize a runner, pass it your suite and run it
-    runner = unittest.TextTestRunner(verbosity=3)
+    runner = unittest.TextTestRunner(verbosity=3, warnings='ignore')
     result = runner.run(suite)
+
+    if len(result.failures):
+        sys.exit(os.EX_SOFTWARE)
