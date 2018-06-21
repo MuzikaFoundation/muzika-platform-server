269fb97780662e5e01b73598dd73c69e27fa7a22
Heo Sangmin <heo@mapiacompany.com>
2018-06-21 19:30:32 +0900

    refactor(board_me): support type parameter

--- controllers/board_me.py
+++ controllers/board_me.py
@@ -11,23 +11,26 @@ from modules.pagination import Pagination
 blueprint = Blueprint('board_me', __name__, url_prefix='/api')
 
 
-@blueprint.route('/board/music/purchased', methods=['GET'])
+@blueprint.route('/user/board/music/purchased', methods=['GET'])
+@jwt_check
 def _get_my_upload_board():
-    buyer_address = request.args.get('buyer_address')
+    type = request.args.get('type')
+    user = request.user
     with db.engine_rdonly.connect() as connect:
         query_str = """
             SELECT 
               `mb`.*, 
-              '!music_contract', `mc`.*,
-              '!music_payment',  `mp`.*
-            FROM `music_board` `mb`
-            INNER JOIN `music_contracts` `mc` ON (`mc`.`post_id` = `mb`.`post_id`)
-            INNER JOIN `music_payments` `mp` ON (`mp`.`contract_address` = `mc`.`contract_address`)
-            WHERE `mp`.`buyer_address` = :buyer_address 
-            ORDER BY `mc`.`contract_address`
+              '!music_contract', `mc`.*, 
+              '!music_payment',  `mp`.* 
+            FROM `music_board` `mb` 
+            INNER JOIN `music_contracts` `mc` ON (`mc`.`post_id` = `mb`.`post_id`) 
+            INNER JOIN `music_payments` `mp` ON (`mp`.`contract_address` = `mc`.`contract_address`) 
+            WHERE `mp`.`buyer_address` = :buyer_address AND `mb`.`type` = :type 
+            ORDER BY `mc`.`contract_address` 
         """
 
-        query = connect.execute(text(query_str), buyer_address=buyer_address)
+        query = connect.execute(text(query_str), type=type,
+                                buyer_address=user['address'])
 
         return helper.response_ok([db.to_relation_model(row) for row in query])
 
